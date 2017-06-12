ec9bf9ca2fe1e03aa4b687ef98f917713dca869d
LukaszKoziarski <lkoziars@gmail.com>
2017-06-12 22:11:35 +0200

    surge stuff

--- src/test/java/exercise/UserFacadeTestIT.java
+++ src/test/java/exercise/UserFacadeTestIT.java
@@ -8,6 +8,9 @@ import exercise.realdatabase.UserFacadeRealDB;
 public class UserFacadeTestIT extends UserFacadeTest {
     @Override
     public IUserFacade makeUserFacade() {
+        if(System.getenv("TRAVIS") != null){
+            return new UserFacadeRealDB("pu_mySql_travis_Integration");
+        }
         return new UserFacadeRealDB("pu_localDB");
     }
 }
